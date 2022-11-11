ec83b9c5ed1e189efb8f72e58e99674b6a6fac35
Oleg Koval <gevin.gl@gmail.com>
2022-11-11 18:37:32 +0200

    Lecture 1 and 2

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,90 @@ function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
     elt.innerText = `Hello from ${name}`;
 }
+
+// ----------------------------------------------------------------
+enum Category {
+    JavaScript,
+    CSS,
+    HTML,
+    TypeScript,
+    Angular,
+}
+
+type Book = {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+};
+
+function getAllBooks(): readonly Book[] {
+    const books = <const>[
+        {
+            id: 1,
+            title: 'Refactoring JavaScript',
+            category: Category.JavaScript,
+            author: 'Evan Burchard',
+            available: true,
+        },
+        {
+            id: 2,
+            title: 'JavaScript Testing',
+            category: Category.JavaScript,
+            author: 'Liang Yuxian Eugene',
+            available: false,
+        },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        {
+            id: 4,
+            title: 'Mastering JavaScript Object-Oriented Programming',
+            category: Category.JavaScript,
+            author: 'Andrea Chiarelli',
+            available: true,
+        },
+    ];
+    return books;
+}
+
+function logFirstAvaible(books: readonly Book[]): void {
+    console.log(`Number of books: ${books.length}`);
+    const title = books.find(book => book?.available)?.title;
+    console.log(title);
+}
+
+function getBookTitlesByCategory(inputCategory: Category): string[] {
+    return getAllBooks()
+        .filter(({ category }) => category === inputCategory)
+        .map(({ title }) => title);
+}
+
+function logBookTitles(titles: Array<string>): void {
+    titles.forEach(title => console.log(title));
+}
+
+function getBookAuthorByIndex(index: number): [title: string, author: string] {
+    const { title, author } = getAllBooks()[index];
+    return [title, author];
+}
+
+function calcTotalPages(): void {
+    const data = <const>[
+        { lib: 'libName1', books: 1_000_000_000, avgPagesPerBook: 250 },
+        { lib: 'libName2', books: 5_000_000_000, avgPagesPerBook: 300 },
+        { lib: 'libName3', books: 3_000_000_000, avgPagesPerBook: 280 },
+    ];
+    const totalPages = data.reduce(
+        (acc: bigint, { books, avgPagesPerBook }) => acc + BigInt(books) * BigInt(avgPagesPerBook),
+        0n,
+    );
+    console.log(totalPages);
+}
+
+// -------------
+// calcTotalPages();
+// console.log(getAllBooks());
+logFirstAvaible(getAllBooks());
+// console.log(getBookTitlesByCategory(Category.CSS));
+// logBookTitles(getBookTitlesByCategory(Category.JavaScript));
+// console.log(getBookAuthorByIndex(0));
